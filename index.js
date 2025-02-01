69aa9c1bf0decd2e827cea1421b1b2112d1ee5fb
Mariana Morales <marianamorales2110@gmail.com>
2025-02-01 16:21:30 -0400

    arreglos

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const Jsonserver = require('json-server');
-const server = Jsonserver.create();
-const router = Jsonserver.router("data.json");
-const middlewares = Jsonserver.defaults();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults();
 const port = 4000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on http://localhost:${port}`);
+});
